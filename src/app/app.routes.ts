ab18c17f81f5dc9193e57b28273aaff8b6abcf85
Shvy-bit <jcusiq@unsa.edu.pe>
2026-06-12 01:19:12 -0500

    Componente start

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,7 @@
 import { Routes } from '@angular/router';
+import { Start } from './start/start';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: '', redirectTo: 'start', pathMatch:'full'},
+    {path:'start', component: Start},
+];
