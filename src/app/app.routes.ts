34d29da325316154c5ba97669367cd21d49b0839
Shvy-bit <jcusiq@unsa.edu.pe>
2026-06-13 09:16:55 -0500

    Enrutamiento del componente game

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,11 @@
 import { Routes } from '@angular/router';
 import { Start } from './start/start';
 import { Difficulty } from './difficulty/difficulty';
+import { Game } from './game/game';
 
 export const routes: Routes = [
     {path: '', redirectTo: 'start', pathMatch:'full'},
     {path:'start', component: Start},
-    {path: 'difficulty', component: Difficulty}
+    {path: 'difficulty', component: Difficulty},
+    {path: 'game', component: Game},
 ];
