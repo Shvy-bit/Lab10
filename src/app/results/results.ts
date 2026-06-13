2856e766a32a86bad4c3495b49ce8cf6269832a1
Shvy-bit <jcusiq@unsa.edu.pe>
2026-06-13 18:41:50 -0500

    Componente Results, semi funcional

--- src/app/results/results.ts
+++ src/app/results/results.ts
@@ -1,9 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { GameService } from '../game-service';
+import { RouterLink } from "@angular/router";
 
 @Component({
   selector: 'app-results',
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './results.html',
   styleUrl: './results.css',
 })
-export class Results {}
+export class Results {
+  private gameService = inject(GameService);
+
+  public gamesMatches = this.gameService.getGamesMatches();
+}
