40683e99352b26e93b26b6473b092495d2f53c73
Shvy-bit <jcusiq@unsa.edu.pe>
2026-06-13 11:49:42 -0500

    enrutamiento de Resuts

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,10 +2,12 @@ import { Routes } from '@angular/router';
 import { Start } from './start/start';
 import { Difficulty } from './difficulty/difficulty';
 import { Game } from './game/game';
+import { Results } from './results/results';
 
 export const routes: Routes = [
     {path: '', redirectTo: 'start', pathMatch:'full'},
     {path:'start', component: Start},
     {path: 'difficulty', component: Difficulty},
     {path: 'game', component: Game},
+    {path: 'results', component: Results}
 ];
